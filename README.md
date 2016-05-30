0a8d40ec65812f0a7ecd92fac461a69ad169b41b
Douglas Gardner <douglas@chippy.ch>
2016-05-30 12:47:04 +0100

    Add first pass for copyleft licenses

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # licensor
 
 Tool to pull a licence from the OSI website and into your new software project.
+
+## Usage
+```
+$ app.py [LICENSE]
+```
+
+Works decently with GPL and related licenses, e.g.
+```
+$ app.py LGPL-3.0
+```
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
@@ -7,22 +7,76 @@
 # Distributed under terms of the MIT license.
 
 """
-Speeds up adding a licence to new software projects.
+Speeds up adding a license to new software projects.
 """
 
+from distutils.util import strtobool
+
+import argparse
 import json
-import urllib
+import sys
+from urllib.request import urlopen
 
 ENDPOINT_LICENSES = 'https://api.opensource.org/licenses/'
 
 
 def pull_licenses():
-    res = urllib.urlopen(ENDPOINT_LICENSES)
-    licenses = json.loads(res.read())
+    res = urlopen(ENDPOINT_LICENSES)
+    with urlopen(ENDPOINT_LICENSES) as res:
+        licenses = json.loads(res.read().decode())
 
     return {x['id']: x for x in licenses}
 
+
+def get_license(name, args):
+    try:
+        license = pull_licenses()[name]
+    except KeyError:
+        sys.exit('Error: license not found!')
+
+    if license['superseded_by']:
+        if prompt_boolean('{0} has been superseded by {1}. Use that instead?'
+           .format(
+                license['name'],
+                license['superseded_by']
+           )
+        ):
+            return get_license(license['superseded_by'])
+
+    text = get_license_text(license)
+    if text:
+        filename = args.file
+        if not filename:
+            if 'copyleft' in license['keywords']:
+                filename = 'COPYING'
+            else:
+                filename = 'LICENSE'
+
+        with open(filename, 'w') as f:
+            f.write(text)
+            print('Wrote contents of {0} to {1}.'.format(license['name'], filename))
+            return 0
+
+
+def get_license_text(license):
+    texts = license['text']
+    for text in texts:
+        if text['media_type'] == 'text/plain':
+            res = urlopen(text['url'])
+            return res.read().decode()
+
+    return None
+
+
+def prompt_boolean(question):
+    return strtobool(input('{0} [y/n]: '.format(question)))
+
+
 if __name__ == '__main__':
-    print(pull_licenses())
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('license', metavar='NAME', help='the license to add')
+    parser.add_argument('--file', metavar='FILE', help='where to write the license', required=False)
+    args = parser.parse_args()
 
+    sys.exit(get_license(args.license, args))
